7025175f8c6b082d207cdeb8878a409e905042e8
BuckeyeDude <buckeyedude92@hotmail.com>
2012-04-07 00:26:36 +0000

    Added code to load files from the command line Fixed issue with disassembly in unicode

--- src/gui/guiapp.cpp
+++ src/gui/guiapp.cpp
@@ -1,6 +1,7 @@
 #include "guiapp.h"
 #include "gui.h"
 #include "wizard/romwizard.h"
+#include "sendfile.h"
 
 WabbitemuFrame *frames[MAX_CALCS];
 
@@ -22,6 +23,8 @@ void WabbitemuApp::LoadSettings(LPCALC lpCalc)
 bool WabbitemuApp::OnInit()
 {
 	wxImage::AddHandler(new wxPNGHandler);
+	//stolen from the windows version
+	ParseCommandLineArgs();
 
 	memset(frames, 0, sizeof(frames));
 	LPCALC lpCalc = calc_slot_new();
@@ -33,11 +36,24 @@ bool WabbitemuApp::OnInit()
 		frame = gui_frame(lpCalc);
 	} else {
 		calc_slot_free(lpCalc);
-		bool success = DoRomWizard();
-		if (!success) {
-			return FALSE;
+		BOOL loadedRom = FALSE;
+		if (parsedArgs.num_rom_files > 0) {
+			for (int i = 0; i < parsedArgs.num_rom_files; i++) {
+				if (rom_load(lpCalc, parsedArgs.rom_files[i])) {
+					gui_frame(lpCalc);
+					loadedRom = TRUE;
+					break;
+				}
+			}
+		}
+		if (!loadedRom) {
+			bool success = DoRomWizard();
+			if (!success) {
+				return FALSE;
+			}
 		}
 	}
+	//LoadCommandlineFiles((INT_PTR) lpCalc, LoadToLPCALC);
 	timer = new wxTimer();
 	timer->Connect(wxEVT_TIMER, (wxObjectEventFunction) &WabbitemuApp::OnTimer);
 	timer->Start(TPF, false);
@@ -57,6 +73,26 @@ void WabbitemuApp::SaveSettings(LPCALC lpCalc) {
 
 int WabbitemuApp::OnExit() {
 	SaveSettings(&calcs[0]);
+	//load ROMs first
+	for (int i = 0; i < parsedArgs.num_rom_files; i++) {
+		free(parsedArgs.rom_files[i]);
+		parsedArgs.rom_files[i] = NULL;
+	}
+	//then archived files
+	for (int i = 0; i < parsedArgs.num_archive_files; i++) {
+		free(parsedArgs.archive_files[i]);
+		parsedArgs.archive_files[i] = NULL;
+	}
+	//then ram
+	for (int i = 0; i < parsedArgs.num_ram_files; i++) {
+		free(parsedArgs.ram_files[i]);
+		parsedArgs.ram_files[i] = NULL;
+	}
+	//finally utility files (label, break, etc)
+	for (int i = 0; i < parsedArgs.num_utility_files; i++) {
+		free(parsedArgs.utility_files[i]);
+		parsedArgs.utility_files[i] = NULL;
+	}
 	return 0;
 }
 
@@ -101,5 +137,98 @@ void WabbitemuApp::OnTimer(wxTimerEvent& event) {
 	// Frame skip if we're too far ahead.
 	} else {
 		difference += TPF;
+	}
+}
+
+void WabbitemuApp::ParseCommandLineArgs()
+{
+	ZeroMemory(&parsedArgs, sizeof(ParsedCmdArgs));
+	TCHAR tmpstring[512];
+	SEND_FLAG ram = SEND_CUR;
+
+	if (argv && argc > 1) {
+		_tcscpy(tmpstring, argv[1]);
+		for(int i = 1; i < argc; i++) {
+			ZeroMemory(tmpstring, 512);
+			_tcscpy(tmpstring, argv[i]);
+			TCHAR secondChar = toupper(tmpstring[1]);
+			if (*tmpstring != '-' && *tmpstring != '/') {
+				TCHAR *temp = (TCHAR *) malloc(_tcslen(tmpstring) + 1);
+				_tcscpy(temp, tmpstring);
+				temp[_tcslen(tmpstring) + 1] = '\0';
+				TCHAR extension[5] = _T("");
+				const TCHAR *pext = _tcsrchr(tmpstring, _T('.'));
+				if (pext != NULL) {
+					_tcscpy(extension, pext);
+				}
+				if (!_tcsicmp(extension, _T(".rom")) || !_tcsicmp(extension, _T(".sav")) || !_tcsicmp(extension, _T(".clc"))) {
+					parsedArgs.rom_files[parsedArgs.num_rom_files++] = temp;
+				}
+				else if (!_tcsicmp(extension, _T(".brk")) || !_tcsicmp(extension, _T(".lab")) 
+					|| !_tcsicmp(extension, _T(".zip")) || !_tcsicmp(extension, _T(".tig"))) {
+						parsedArgs.utility_files[parsedArgs.num_utility_files++] = temp;
+				}
+				else if (ram) {
+					parsedArgs.ram_files[parsedArgs.num_ram_files++] = temp;
+				} else {
+					parsedArgs.archive_files[parsedArgs.num_archive_files++] = temp;
+				}
+			} else if (secondChar == 'R') {
+				ram = SEND_RAM;
+			} else if (secondChar == 'A') {
+				ram = SEND_ARC;
+			} else if (secondChar == 'S') {
+				parsedArgs.silent_mode = TRUE;
+			} else if (secondChar == 'F') {
+				parsedArgs.force_focus = TRUE;
+			} else if (secondChar == 'N') {
+				parsedArgs.force_new_instance = TRUE;
+			}
+		}
+	}
+}
+
+/*void LoadAlreadyExistingWabbit(unsigned int lParam, LPTSTR filePath, SEND_FLAG sendLoc)
+{
+	HWND hwnd = (HWND) lParam;
+	COPYDATASTRUCT *cds = (COPYDATASTRUCT *) malloc(sizeof(COPYDATASTRUCT));
+	cds->dwData = sendLoc;
+	size_t strLen;
+	cds->lpData = filePath;
+	if (PathIsRelative(filePath)) {
+		TCHAR tempPath[MAX_PATH];
+		TCHAR *tempPath2 = (TCHAR *) malloc(MAX_PATH);
+		_tgetcwd(tempPath, MAX_PATH);
+		PathCombine(tempPath2, tempPath, filePath);
+		cds->lpData = tempPath2;
+	}
+	StringCbLength(filePath, 512, &strLen);
+	cds->cbData = strLen;
+	SendMessage(hwnd, WM_COPYDATA, (WPARAM) NULL, (LPARAM) cds);
+}*/
+
+void LoadToLPCALC(INT_PTR lParam, LPTSTR filePath, SEND_FLAG sendLoc)
+{
+	LPCALC lpCalc = (LPCALC) lParam;
+	SendFile(lpCalc, filePath, sendLoc);
+}
+
+void WabbitemuApp::LoadCommandlineFiles(INT_PTR lParam,  void (*load_callback)(INT_PTR, LPTSTR, SEND_FLAG))
+{
+	//load ROMs first
+	for (int i = 0; i < parsedArgs.num_rom_files; i++) {
+		load_callback(lParam, parsedArgs.rom_files[i], SEND_ARC);
+	}
+	//then archived files
+	for (int i = 0; i < parsedArgs.num_archive_files; i++) {
+		load_callback(lParam, parsedArgs.archive_files[i], SEND_ARC);
+	}
+	//then ram
+	for (int i = 0; i < parsedArgs.num_ram_files; i++) {
+		load_callback(lParam, parsedArgs.ram_files[i], SEND_RAM);
+	}
+	//finally utility files (label, break, etc)
+	for (int i = 0; i < parsedArgs.num_utility_files; i++) {
+		load_callback(lParam, parsedArgs.utility_files[i], SEND_ARC);
 	}
 }
--- src/gui/guiapp.h
+++ src/gui/guiapp.h
@@ -3,6 +3,24 @@
 #include "calc.h"
 #include <wx/config.h>
 
+
+#define MAX_FILES 255
+typedef struct ParsedCmdArgs
+{
+	LPTSTR rom_files[MAX_FILES];
+	LPTSTR utility_files[MAX_FILES];
+	LPTSTR archive_files[MAX_FILES];
+	LPTSTR ram_files[MAX_FILES];
+	int num_rom_files;
+	int num_utility_files;
+	int num_archive_files;
+	int num_ram_files;
+	BOOL silent_mode;
+	BOOL force_new_instance;
+	BOOL force_focus;
+} ParsedCmdArgs_t;
+
+
 class WabbitemuApp: public wxApp
 {
 private:
@@ -15,6 +33,12 @@ private:
 	wxConfigBase *settingsConfig;
 	wxTimer *timer;
 	unsigned GetTickCount();
+	
+	ParsedCmdArgs_t parsedArgs;
+	void ParseCommandLineArgs();
+	void LoadCommandlineFiles(INT_PTR,  void (*load_callback)(INT_PTR, LPTSTR, SEND_FLAG));
 public:
 	static BOOL DoRomWizard();
 };
+
+void LoadToLPCALC(INT_PTR lParam, LPTSTR filePath, SEND_FLAG sendLoc);
